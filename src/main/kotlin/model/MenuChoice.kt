f91660eb632a02d9f1c69750bb043b0f878cbb45
Jose Garza <sventripikal@gmail.com>
2025-02-14 14:02:59 -0600

    ENUM UPDATES
    -added printException & printCustomException functions for printing exceptions
    -created CONSTANTS file & added all constants
    -added string options to MenuChoice enum
    -updated promptMenuChoice function
    
    Update MovieTheatre.kt

--- src/main/kotlin/model/MenuChoice.kt
+++ src/main/kotlin/model/MenuChoice.kt
@@ -1,13 +1,21 @@
 package cinema.model
 
+import cinema.ONE
+import cinema.TWO
+import cinema.ZERO
+
+private const val MENU_ONE = "$ONE. Show the seats"
+private const val MENU_TWO = "$TWO. Buy a ticket"
+private const val MENU_ZERO = "$ZERO. Exit"
+
 /**
  *  enum class replacing user input for MenuChoice
  *  @property SHOW_SEATS print theatre to screen
  *  @property BUY_TICKET update available seats
  *  @property EXIT end program
  */
-enum class MenuChoice {
-    SHOW_SEATS,
-    BUY_TICKET,
-    EXIT
+enum class MenuChoice(val option: String) {
+    SHOW_SEATS(MENU_ONE),
+    BUY_TICKET(MENU_TWO),
+    EXIT(MENU_ZERO)
 }
