1c5281aef0c408a97175abf51a1377da0c9251b2
Jose Garza <sventripikal@gmail.com>
2025-02-14 14:44:41 -0600

    STATISTICS ADDED
    -additional constant values
    -added Statistics menu choice
    -updated MovieTheatre to include Current Income & Possible Income properties
    -updated how ticket prices are received
    -Created functions to update income properties & print statistics
    
    MENU CHOICE - STATISTICS
    
    COMMENTS
    
    CONSTANT UPDATE

--- src/main/kotlin/model/MenuChoice.kt
+++ src/main/kotlin/model/MenuChoice.kt
@@ -1,21 +1,26 @@
 package cinema.model
 
 import cinema.ONE
+import cinema.THREE
 import cinema.TWO
 import cinema.ZERO
+import cinema.model.MenuChoice.*
 
 private const val MENU_ONE = "$ONE. Show the seats"
 private const val MENU_TWO = "$TWO. Buy a ticket"
+private const val MENU_THREE = "$THREE. Statistics"
 private const val MENU_ZERO = "$ZERO. Exit"
 
 /**
  *  enum class replacing user input for MenuChoice
  *  @property SHOW_SEATS print theatre to screen
  *  @property BUY_TICKET update available seats
+ *  @property STATISTICS print income/occupancy/percentage stats
  *  @property EXIT end program
  */
 enum class MenuChoice(val option: String) {
     SHOW_SEATS(MENU_ONE),
     BUY_TICKET(MENU_TWO),
+    STATISTICS(MENU_THREE),
     EXIT(MENU_ZERO)
 }
